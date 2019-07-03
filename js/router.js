7395db9cb492a6dedd94d139a1116356fd795a1b
kvanotten <46548047+kvanotten@users.noreply.github.com>
2019-07-02 19:14:04 -0600

    Add files via upload

---
+++ js/router.js
@@ -0,0 +1,16 @@
+
+var view = "view_landing";
+
+switch(view) {
+  case "view_landing":
+    // code block
+        console.log("Show the landing UI view");
+    break;
+  case "view_forecast":
+    // code block
+        console.log("Show the forecast UI view");
+    break;
+  default:
+    // code block
+        console.log("Error: The requested view is not recognized by the router");
+}
--- js/variable.js
+++ js/variable.js
@@ -6,4 +6,32 @@ a multi line
 comment
 */ 
 
-console.log("Hello World!");
+console.log("Reading Challenge");
+
+//UI Views
+var view_landing = document.getElementById("view_landing");
+console.log(view_landing);
+
+var view_content = document.getElementById("view_content");
+console.log(view_content);
+
+// UI Components (Nav Icons)
+var input_ico_locate = document.getElementById("input_ico_locate");
+//console.log(input_ico_locate);
+
+// UI Component (Buttons)
+var input_btn_reading_log = document.getElementById("input_btn_reading_log");
+console.log(input_btn_reading_log);
+
+var dummy_section = document.getElementById("dummy_section");
+
+input_btn_reading_log.addEventListener("click",function(){
+   console.log("User click on button reading log."); 
+    hide_all_view();
+    dummy_section.classList.remove("d-none");
+    
+});
+
+function hide_all_view(){
+    dummy_section.classList.add("d-none");
+}
